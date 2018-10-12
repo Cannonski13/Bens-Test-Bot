a50b3ba25f70c1fdd83714923fcb4e18fd9b6ee8
Cannonski13 <43939704+Cannonski13@users.noreply.github.com>
2018-10-12 15:40:14 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,14 @@
-const Eris = require('eris');
-const bot = new Eris(process.env.BOT_TOKEN);
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log('Connected');
-    bot.editStatus('online', {name: 'SetPrefix ?', type :0})
+client.on('ready', () => {
+  console.log(`I am Ready!`);
 });
 
-bot.on('messageCreate', msg => {
-    const usedby = `Command ${msg.content} used by ${msg.member.username}#${msg.member.discriminator} (${msg.member.id})`;
-
-    if (msg.author.bot) return;
-
-    if (msg.content === '!updates') {
-        bot.createMessage(msg.channel.id, {embed: {
-            description: 'Our list of updates can be found in the Official Server (Run command !help to join)'
-        }})
-        .catch((e) => {console.log(e)});
-
-        console.log(usedby);
-    };
-        
-    if (msg.content === '!commands') {
-        bot.createMessage(msg.channel.id, {embed: {
-            description: 'Commands can be found here:'
-        }})
-        .catch((e) => {console.log(e)});
-
-        console.log(usedby);
-    };
-
-    if (msg.content === '!help') {
-        bot.createMessage(msg.channel.id, {embed: {
-            description: 'If you require help than please join the support server here:'
-        }})
-        .catch((e) => {console.log(e)});
-
-        console.log(usedby);
-    };
-      
-    if (msg.content === '!addme') {
-        bot.createMessage(msg.channel.id, {embed: {
-            description: 'You can add me here: '
-        }})
-        .catch((e) => {console.log(e)});
-
-        console.log(usedby);
-      };
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-bot.connect();
+client.login(process.env.BOT_TOKEN);
